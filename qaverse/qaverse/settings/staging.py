40fb472c8fcfb5846ef4481371670629da5c40c3
Muhammad Zohaib <mzohaib01.inbox@gmail.com>
2025-10-03 22:33:53 +0500

    Add PostgreSQL configuration

--- qaverse/qaverse/settings/staging.py
+++ qaverse/qaverse/settings/staging.py
@@ -1,4 +1,6 @@
 from .base import *
+import os
+import dj_database_url
 
 DEBUG = True
 ALLOWED_HOSTS = ["qaverse-staging.up.railway.app", "localhost"]
@@ -9,10 +11,11 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
