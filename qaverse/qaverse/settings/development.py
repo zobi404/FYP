da920aad63e86b8c94d50605795860ed3d412698
Muhammad Zohaib <mzohaib01.inbox@gmail.com>
2026-01-31 16:33:15 +0500

    check now

--- qaverse/qaverse/settings/development.py
+++ qaverse/qaverse/settings/development.py
@@ -13,7 +13,8 @@ DATABASES = {
 # Email Settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = env.int('EMAIL_PORT', 587)
+EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', True)
+EMAIL_USE_SSL = env.bool('EMAIL_USE_SSL', False)
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
--- qaverse/qaverse/settings/staging.py
+++ qaverse/qaverse/settings/staging.py
@@ -32,7 +32,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # Email Settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = env.int('EMAIL_PORT', 587)
+EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', True)
+EMAIL_USE_SSL = env.bool('EMAIL_USE_SSL', False)
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
