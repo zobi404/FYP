5d5e660f0775be7c16d0fa84a2e7a4f89d528dfb
Muhammad Zohaib <mzohaib01.inbox@gmail.com>
2026-01-31 16:20:50 +0500

    build

--- qaverse/qaverse/settings/staging.py
+++ qaverse/qaverse/settings/staging.py
@@ -28,3 +28,11 @@ DATABASES = {
 }
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# Email Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
