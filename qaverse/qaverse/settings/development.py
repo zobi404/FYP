35c4a8544d9776a71b9f40b84f82e3c8546b32d2
malikumair5 <malikumair.dev@gmail.com>
2026-01-31 21:36:51 +0500

    changes in email config

--- qaverse/qaverse/settings/development.py
+++ qaverse/qaverse/settings/development.py
@@ -13,7 +13,8 @@ DATABASES = {
 # Email Settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
