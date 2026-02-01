ee92dc5218dd99bdcc23afd8dd9f6b5b9be8bb2f
Muhammad Zohaib <mzohaib01.inbox@gmail.com>
2026-02-01 20:55:41 +0500

    Updated pagination for maintained api

--- qaverse/projects/views.py
+++ qaverse/projects/views.py
@@ -6,7 +6,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from projects.models import Project
 from projects.serializers import ProjectSerializer
 from projects.permissions import IsMaintainerOrReadOnly
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 
 @extend_schema(tags=['Projects'])
 class ProjectViewSet(viewsets.ModelViewSet):
@@ -24,6 +24,12 @@ class ProjectViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(maintainer=self.request.user)
 
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(name='page', description='Page number', required=False, type=int),
+        ],
+        responses={200: ProjectSerializer(many=True)}
+    )
     @action(detail=False, methods=['get'], permission_classes=[permissions.IsAuthenticated])
     def maintained(self, request):
         projects = Project.objects.filter(maintainer=request.user).select_related('maintainer').annotate(
