5e7fd96ea54d14b5ad47cbb16dfca73ec88873fb
Muhammad Zohaib <mzohaib01.inbox@gmail.com>
2026-02-02 01:13:37 +0500

    [Bug Fix] -> Project viewset

--- qaverse/projects/urls.py
+++ qaverse/projects/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import ProjectViewSet
 
 router = DefaultRouter()
-router.register(r'projects', ProjectViewSet)
+router.register(r'projects', ProjectViewSet, basename='project')
 
 urlpatterns = [
     path('', include(router.urls)),
